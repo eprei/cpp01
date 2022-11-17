0b8f1a0171cfc4a4517330e624c3d1859dd66289
Emiliano Presa custodio <epresa-c@c2r8s9.42lausanne.ch>
2022-11-17 10:55:50 +0100

    ex04 ok

--- ex04/Replacer.cpp
+++ ex04/Replacer.cpp
@@ -11,20 +11,36 @@ bool	Replacer::getArgs(char **argv)
 	this->_inputFileName = argv[1];
 	this->_stringToReplace = argv[2];
 	this->_newString = argv[3];
-
 	this->_ifs.open(this->_inputFileName.data());
+	this->_outputFileName = this->_inputFileName;
+	this->_outputFileName += ".replace";
 	return (this->_ifs.is_open());
 }
 
 void	Replacer::execReplacement(void)
 {
-	this->_outputFileName = this->_inputFileName;
-	this->_outputFileName += ".replace";
+	std::string	newLine;
+	std::string	lineReplaced;
+	std::size_t	found;
 
-	std::cout << "input fileName = " << this->_inputFileName << std::endl;
-	std::cout << "output fileName = " << this->_outputFileName << std::endl;
-	std::cout << "string to replace = " << this->_stringToReplace << std::endl;
-	std::cout << "new string = " << this->_newString << std::endl;
+	this->_ofs.open(this->_outputFileName.data());
+	getline(this->_ifs, newLine);
+	found = newLine.find(this->_stringToReplace);
+	while (this->_ifs.eof() == false)
+	{
+		while (found != std::string::npos)
+		{
+			newLine.insert(found, this->_newString);
+			newLine.erase(found + this->_newString.length(), this->_stringToReplace.length());
+			found = newLine.find(this->_stringToReplace, found + this->_newString.length());
+		}
+		this->_ofs << newLine << std::endl;
+		getline(this->_ifs, newLine);
+		while (newLine.compare("\n") == 0)
+			getline(this->_ifs, newLine);
+		found = newLine.find(this->_stringToReplace);
+	}
+	this->_ofs.close();
 	this->_ifs.close();
 	return ;
 }
